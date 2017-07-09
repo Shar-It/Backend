f6f6809d92393bf9cb6675669e25c4df48318d1c
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-07-09 15:26:01 +0200

    Init

---
+++ src/main/kotlin/com/alorma/sharit/SharitApplication.kt
@@ -0,0 +1,11 @@
+package com.alorma.sharit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SharitApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SharitApplication::class.java, *args)
+}
