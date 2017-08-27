f6ac889e687f718fb0996b1d0bb0cbb872f9dd08
Bernat Borrás <bernatbor15@gmail.com>
2017-08-27 19:34:13 +0200

    Fix unused imports

--- src/main/kotlin/com/alorma/sharit/controller/UserController.kt
+++ src/main/kotlin/com/alorma/sharit/controller/UserController.kt
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import javax.servlet.http.HttpServletResponse
 
 @RestController
 @RequestMapping("/users")
