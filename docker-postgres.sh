ab66b8737043322b9ad215cab2c71c5089fb2bee
Bernat Borrás <bernatbor15@gmail.com>
2017-08-27 16:49:43 +0200

    NO Docker

--- docker-postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --detach \
-  --name postgres-db \
-  --env POSTGRES_PASSWORD=password \
-  --env POSTGRES_USER=postgres \
-  --publish 5432:5432 \
-  postgres:9.4.4
--- src/main/kotlin/com/alorma/sharit/SharitApplication.kt
+++ src/main/kotlin/com/alorma/sharit/SharitApplication.kt
@@ -1,11 +1,13 @@
 package com.alorma.sharit
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ComponentScan("com.alorma.sharit")
+@EnableAutoConfiguration
 class SharitApplication
 
 fun main(args: Array<String>) {
